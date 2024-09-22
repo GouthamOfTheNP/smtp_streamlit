2821115a1b24d7fbb741cbf37c0692c40e3b70da
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-09-22 18:20:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from send_email import send_email
+from datetime import datetime
+import time as t
+
+title = st.title("Mass Email Sender")
+
+with st.form(key="my_form"):
+	recipient = st.text_input("Recipients", help="Use commas to separate email addresses")
+	recipients = [email.strip() for email in recipient.split(",") if email]
+	subject = st.text_input("Subject")
+	content = st.text_area("Content")
+	schedule = st.checkbox("Scheduled email send")
+	date = st.date_input("Date to send")
+	time = st.time_input("Time to send")
+	if st.form_submit_button("Send"):
+		recipients_sent = []
+		if date and time:
+			schedule_time = datetime.combine(date, time)
+			current_datetime = datetime.now()
+			if current_datetime >= schedule_time:
+				for recipient in recipients:
+					send_email(subject, content, recipient)
+					recipients_sent.append(recipient)
+				if len(recipients_sent) == len(recipients):
+					st.success("All emails have been sent successfully!")
+					if st.success:
+						t.sleep(5)
+						st.rerun()
+		else:
+			for recipient in recipients:
+				send_email(subject, content, recipient)
+				recipients_sent.append(recipient)
+			if len(recipients_sent) == len(recipients):
+				st.success("All emails have been sent successfully!")
+				if st.success:
+					t.sleep(5)
+					st.rerun()
